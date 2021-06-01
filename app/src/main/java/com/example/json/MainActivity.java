091a956e475f66d3a446216b1f175f36f51b2017
Francisco Canto <2160781@my.ipleiria.pt>
2021-06-01 16:26:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/json/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> numberlist = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        get_jason();
+    }
+
+    public void get_jason(){
+
+        String json;
+        try{
+            InputStream is = getAssets().open("JsonTest");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+            JSONArray jsonArray = new JSONArray(json);
+
+            for(int i = 0; i<jsonArray.length();i++){
+                JSONObject obj = jsonArray.getJSONObject(i);
+
+                if (obj.getString("city").equals("isb")){
+                    numberlist.add(obj.getString("number"));
+                }
+
+            }
+
+        Toast.makeText(getApplicationContext(),numberlist.toString(),Toast.LENGTH_LONG).show();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+}
